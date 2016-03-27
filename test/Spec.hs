c2962bb0c06278c3ad8f83798d59b39c530703da
Przemysław Kopański <P.Kopanski@stud.elka.pw.edu.pl>
2016-03-27 23:03:05 +0200

    Added parser tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.Hspec
+
+import Data.Text (Text, pack)
+
+import Text.Parsec (runParser)
+
+import Lib
+
+toChord :: Text -> Chord
+toChord input =
+  case runParser oneChord "" "" input of
+    (Left _) -> Chord (-1)
+    (Right c) -> c
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "parser validator" $ do
+    it "parser should parse all possible chords" $ do
+      unChord . toChord . pack <$> allChords `shouldBe` [0..35]
